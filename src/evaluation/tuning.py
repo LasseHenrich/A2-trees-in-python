1f8d40fc7cf88333d5e1070700b36a69bbeda6a0
Lasse Henrich <lasse.finn.henrich@gmail.com>
2026-06-25 22:28:03 +0200

    d

--- src/evaluation/tuning.py
+++ src/evaluation/tuning.py
@@ -7,6 +7,7 @@ DO NOT MODIFY THE FUNCTION SIGNATURES.
 """
 
 from itertools import product
+import statistics
 
 from decision_tree.tree import DecisionTreeClassifier
 from evaluation.metrics import accuracy, confusion_matrix
@@ -41,8 +42,33 @@ def stopping_search(
         }
     """
     
-    pass
+    X_tuples = [tuple(feature_as_list) for feature_as_list in X]
     
+    stop_depths_unique = list(dict.fromkeys(stop_depths))
+    stop_belows_unique = list(dict.fromkeys(stop_belows))
+    
+    dicts = []
+    for stop_depth in stop_depths_unique:
+        for stop_below in stop_belows_unique:
+            pred_result = tree.predict_with_depth(X=X_tuples, stop_depth=stop_depth, stop_below=stop_below)
+            
+            predictions = [entry[0] for entry in pred_result]
+            depths = [entry[1] for entry in pred_result]
+            
+            avg_pred_depth = statistics.mean(depths)
+            acc = accuracy(y_true=y, y_pred=predictions)
+            conf_mat_result = confusion_matrix(y_true=y, y_pred=predictions)
+            conf_mat = conf_mat_result[1]
+            
+            dicts.append({
+                "stop_depth": stop_depth,
+                "stop_below": stop_below,
+                "accuracy": acc,
+                "confusion_matrix": conf_mat,
+                "avg_pred_depth": avg_pred_depth
+            })
+            
+    return dicts
     
     
 def grid_search(
@@ -82,4 +108,27 @@ def grid_search(
         }
     """
     
-    pass
+    dicts = []    
+    for criterion in grid["criterion"]:
+        max_depth = max(grid["stop_depth"]) if None not in grid["stop_depth"] else None
+        min_samples_split = min(grid["stop_below"]) if None not in grid["stop_below"] else None
+        
+        if min_samples_split is None:
+            tree = DecisionTreeClassifier(criterion=criterion, max_depth=max_depth)
+        else:
+            tree = DecisionTreeClassifier(criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split)
+            
+        tree.fit(X_train, y_train)
+        
+        results = stopping_search(tree=tree, X=X_val, y=y_val, stop_depths=grid["stop_depth"], stop_belows=grid["stop_below"])
+        
+        for result in results:
+            dicts.append({
+                "criterion": criterion,
+                "stop_depth": result["stop_depth"],
+                "stop_below": result["stop_below"],
+                "accuracy": result["accuracy"],
+                "confusion_matrix": result["confusion_matrix"]
+            })
+            
+    return dicts
